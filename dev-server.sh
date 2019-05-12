288cfeebbf1c6ab9103bf226bf52f01df9a30d05
ZenBlender <sbalay@gmail.com>
2019-05-12 07:43:51 -0600

    try using watchPosition()

---
+++ dev-server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+npx parcel-bundler index.html --https --out-dir dist-dev-server
--- index.ts
+++ index.ts
@@ -5,16 +5,22 @@ function success(position) {
   const longitude = position.coords.longitude
 
   const linkLabel = `Latitude: ${latitude} °, Longitude: ${longitude} °`
-  el.innerHTML = `<a href="https://www.openstreetmap.org/#map=18/${latitude}/${longitude}">${linkLabel}</a>`
+  const linkHTML = `<a href="https://www.openstreetmap.org/#map=18/${latitude}/${longitude}">${linkLabel}</a>`
+
+  const newEl = document.createElement('div')
+  newEl.innerHTML = linkHTML
+  el.appendChild(newEl)
 }
 
 function error(positionError: PositionError) {
-  el.textContent = `Failed to get location (${JSON.stringify(positionError)}).`
+  const newEl = document.createElement('div')
+  newEl.innerHTML = `Failed to get location (${JSON.stringify(positionError)}).`
+  el.appendChild(newEl)
 }
 
 if (!navigator.geolocation) {
   el.textContent = 'Geolocation is not supported by your browser'
 } else {
   el.textContent = 'Locating...'
-  navigator.geolocation.getCurrentPosition(success, error)
+  navigator.geolocation.watchPosition(success, error)
 }
